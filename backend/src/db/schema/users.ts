d65fae770d0d37b9021db24305abd743250712b0
John Cedric Panti <inhog.cedrick@gmail.com>
2025-01-30 21:43:38 +0800

    Define user schema and set up database connection
    * Created the user schema using Drizzle ORM.
    * Established a database connection with `drizzle` and `node-postgres`.

---
+++ backend/src/db/schema/users.ts
@@ -0,0 +1,17 @@
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: integer().primaryKey(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  bio: text(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+});
