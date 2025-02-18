3da4879a430f3093d8417191557efb31e9baf690
John Cedric Panti <inhog.cedrick@gmail.com>
2025-02-18 15:12:43 +0800

    Update user schema with role and password columns
    * Added `role` and `password` columns to the user schema.
    * Updated schema types to use `zod` for validation.

--- server/src/db/schema/users.ts
+++ server/src/db/schema/users.ts
@@ -1,11 +1,26 @@
-import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+import { z } from "zod";
+
+export const roles = pgEnum("roles", ["user", "admin"]);
 
 export const users = pgTable("users", {
   id: serial().primaryKey(),
   email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }),
   googleId: varchar({ length: 255 }).notNull(),
   name: varchar({ length: 255 }).notNull(),
   bio: text(),
+  role: roles().$default(() => "user"),
   refreshToken: text(),
   createdAt: timestamp().defaultNow(),
   updatedAt: timestamp()
@@ -13,5 +28,24 @@ export const users = pgTable("users", {
     .$onUpdate(() => new Date()),
 });
 
-export type InsertUser = typeof users.$inferInsert;
-export type SelectUser = typeof users.$inferSelect;
+export const selectUsersSchema = createSelectSchema(users);
+
+export const insertUserGoogleSchema = createInsertSchema(users, {
+  email: z.string().email(),
+  name: z.string().min(1),
+  googleId: z.string().min(1),
+}).omit({
+  password: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const insertUserEmailSchema = createInsertSchema(users, {
+  email: z.string().email(),
+  name: z.string().min(1),
+  password: z.string().min(8),
+}).omit({
+  googleId: true,
+  createdAt: true,
+  bio: true,
+});
